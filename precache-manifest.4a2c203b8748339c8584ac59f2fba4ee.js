d30b4f3d02bf99ea03dd74ac846f7a7cf82431e1
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-19 19:40:09 +0000

    Updates

---
+++ precache-manifest.4a2c203b8748339c8584ac59f2fba4ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa15e98d475e1edfea57b87e0bc588f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6a61f9b0c624e33cce63",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d903c4e79226cc5c5da9",
+    "url": "/my-app/static/js/2.ce0dac48.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.ce0dac48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a61f9b0c624e33cce63",
+    "url": "/my-app/static/js/main.db050e93.chunk.js"
+  },
+  {
+    "revision": "e7f7024091211f921287",
+    "url": "/my-app/static/js/runtime-main.34190ac4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
